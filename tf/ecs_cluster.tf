fb067f3af0824edd69c258b12d476495bc65db64
Mark Winterbottom <mark@londonappdeveloper.com>
2018-01-27 18:04:13 +0000

    Create main.

--- tf/ecs_cluster.tf
+++ tf/ecs_cluster.tf
@@ -1,17 +1,3 @@
-
-terraform {
-  backend "s3" {
-    bucket = "c2-state-files"
-    key    = "c2-hello-world-api/tf.state"
-    region = "us-east-1"
-  }
-  required_version = "> 0.11.0, < 0.12"
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_ecs_cluster" "main" {
   name = "${var.prefix}-cluster"
 }
---
+++ tf/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "c2-state-files"
+    key    = "c2-hello-world-api/tf.state"
+    region = "us-east-1"
+  }
+  required_version = "> 0.11.0, < 0.12"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
