db9f72f337bdcd37687f52734a956c32ebd071a0
Mark Winterbottom <mark@londonappdeveloper.com>
2018-08-22 08:45:05 +0100

    Changed region.

--- tf/main.tf
+++ tf/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "c2-state-files"
     key    = "c2-hello-world-api/tf.state"
-    region = "us-east-1"
+    region = "eu-west-2"
   }
   required_version = "> 0.11.0, < 0.12"
 }
